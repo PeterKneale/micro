dfc4a3e808ff63ce39ca634d570d642440855cb2
peterkneale <peterkneale@gmail.com>
2019-04-25 22:03:56 +1000

    fix connection strings

--- test.sh
+++ test.sh
@@ -11,11 +11,11 @@ echo "Running unit tests for content"
 docker-compose -f docker-compose-tests-unit-content.yml up --build --force-recreate
 
 echo "Running integration tests for tenants"
-docker-compose -f docker-compose-tests-integration-tenants.yml up --build --force-recreate --exit-code-from micro.services.tenants.integrationtests
+docker-compose -f docker-compose-tests-integration-tenants.yml up --build --force-recreate --exit-code-from micro.services.tenants.integrationtests --abort-on-container-exit
 
 echo "Running integration tests for content"
-docker-compose -f docker-compose-tests-integration-content.yml up --build --force-recreate --exit-code-from micro.services.content.integrationtests
+docker-compose -f docker-compose-tests-integration-content.yml up --build --force-recreate --exit-code-from micro.services.content.integrationtests --abort-on-container-exit
 
 echo "Running acceptance tests"
-docker-compose -f docker-compose-tests-acceptance.yml up --build --force-recreate --exit-code-from micro.services.acceptancetests
+docker-compose -f docker-compose-tests-acceptance.yml up --build --force-recreate --exit-code-from micro.services.acceptancetests --abort-on-container-exit
 

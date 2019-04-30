1a4b30d678552bd1ab6cd577cef8be329350ba16
peterkneale <peterkneale@gmail.com>
2019-04-30 22:02:39 +1000

    fix k8s

--- k8s/install_app.sh
+++ k8s/install_app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-kubectl apply -f app.yml    --namespace micro-dev
-kubectl apply -f app.yml    --namespace micro-qa
-kubectl apply -f app.yml    --namespace micro-prod
+#kubectl delete namespace micro
+#kubectl create namespace micro
+kubectl apply -f inf.yml --namespace micro
+kubectl apply -f app.yml --namespace micro

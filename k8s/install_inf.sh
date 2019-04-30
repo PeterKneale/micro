c31d1ca7e66fbfc5369b40aa07659d3bcfd1ec12
peterkneale <peterkneale@gmail.com>
2019-04-30 22:02:39 +1000

    fix k8s

--- k8s/install_inf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-kubectl delete pods             --namespace micro-inf --all
-kubectl delete deployments      --namespace micro-inf --all
-kubectl delete services         --namespace micro-inf --all
-kubectl delete ingress          --namespace micro-inf --all
-kubectl apply -f inf.yml        --namespace micro-inf
--- k8s/install_ns.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-kubectl create namespace micro-inf
-kubectl create namespace micro-dev
-kubectl create namespace micro-qa
-kubectl create namespace micro-prod
